69371c8cec02af8afff29c70932bf593c084ebfc
josephsteve <josephsteve08@gmail.com>
2019-06-04 19:48:02 +0000

    changes

--- open_academy/__manifest__.py
+++ open_academy/__manifest__.py
@@ -3,8 +3,7 @@
     'name': "Open Academy",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        My very first module created for Odoo""",
 
     'description': """
         Long description of module's purpose
