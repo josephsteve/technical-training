e21bd79d333a85c16f4523a039f7bffa067f35b2
Joseph Serrano <josephsteve08@gmail.com>
2019-06-05 09:31:01 -0700

    change name property to compute fields

--- open_academy/models/person.py
+++ open_academy/models/person.py
@@ -7,16 +7,16 @@ class Person(models.Model):
     _name = 'open_academy.person'
     _description = 'Person Information'
 
-    # name = fields.Char(compute='_compute_name')
+    name = fields.Char(compute='_compute_name')
 
     first_name = fields.Char(string='First Name', required=True)
     last_name = fields.Char(string='Last Name', required=True)
 
-    # @api.depends('first_name', 'last_name')
-    # def _compute_name(self):
-    #     for record in self:
-    #         record.name = f'{record.first_name} {record.last_name}'
+    @api.depends('first_name', 'last_name')
+    def _compute_name(self):
+        for record in self:
+            record.name = f'{record.first_name} {record.last_name}'
 
-    @api.multi
-    def name_get(self):
-        return [(record.id, f'{record.first_name} {record.last_name}') for record in self]
+    # @api.multi
+    # def name_get(self):
+    #     return [(record.id, f'{record.first_name} {record.last_name}') for record in self]
